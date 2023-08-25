b9baf518d7810c69706eeadb1a2c15a3943915c9
nabin25 <paudyalnabin25@gmail.com>
2023-08-25 13:06:31 +0545

    added more files

--- preprocessing/pre-process.py
+++ preprocessing/pre-process.py
@@ -28,8 +28,8 @@ mp_hands = mp.solutions.hands
 hands_detector = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
 
 # Load the video
-video_path = "E:/signs/NSL23/S2_NSL_Consonant_Bright/S2_JHA.MOV"
-output_folder = "E:/major-project/pre-images/pre-for-jha"
+video_path = "E:/signs/NSL23/S1_NSL_consonent_Dark_Cropped/S1_DA.mov"
+output_folder = "E:/major-project/pre-images/pre-for-da"
 frame_skip_interval = 5
 
 # Create output folder if it doesn't exist
@@ -39,7 +39,7 @@ if not os.path.exists(output_folder):
 cap = cv2.VideoCapture(video_path)
 
 frame_count = 0
-file_counter = 64
+file_counter = 57
 
 while True:
     # Read the next frame
