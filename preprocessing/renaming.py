8ce5f78cf3cf973cfd2ee1a8831a7643109603c4
nabin25 <paudyalnabin25@gmail.com>
2023-12-21 19:45:29 +0545

    Much more commits to do

--- preprocessing/renaming.py
+++ preprocessing/renaming.py
@@ -1,7 +1,10 @@
 import os
 
 # Specify the folder path where your files are located
-folder_path = "E:/major-project/pre-images/pre-for-yan"
+folder_path = "E:/major-project/pre-images/pre-for-all2"
+
+# Set the starting value for renaming
+starting_value = 3664  # Change this to your desired starting value
 
 # Get a list of all files in the folder
 file_list = os.listdir(folder_path)
@@ -12,9 +15,9 @@ file_list = [file for file in file_list if file.startswith('hand_') and file[5:9
 # Sort the files by their numeric part
 file_list.sort(key=lambda x: int(x[5:9]))
 
-# Rename the files with sequential numbers
+# Rename the files with sequential numbers starting from the specified value
 for i, file_name in enumerate(file_list):
-    new_name = f'hand_{i:04d}.png'  # Replace 'extension' with the actual file extension
+    new_name = f'hand_{starting_value + i:04d}.png'  # Replace 'extension' with the actual file extension
     old_path = os.path.join(folder_path, file_name)
     new_path = os.path.join(folder_path, new_name)
     
